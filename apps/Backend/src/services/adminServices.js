70f9c0518bce57e87ffbb0717146c238a713d859
Naveen Beniwal <naveenbeniwal00001@gmail.com>
2024-12-31 22:46:43 +0530

    back the changes in amin schema

--- apps/Backend/src/services/adminServices.js
+++ apps/Backend/src/services/adminServices.js
@@ -1,34 +1,20 @@
-import User from "../schema/userSchema.js";
-import apiResponse from "../utils/apiResponse.js";
-import Student from "../schema/student/studentSchema.js";
-// locking the students personalinfo and acdemics
-export default class AdminServices{
-      constructor(studentModel, userModel) {
-        this.student = Student;
-        this.user = userModel;
-    }
-//locking the students personalinfo and acdemics
-async verifyAndLockStudent(studentId, adminId) {
-  try {
-    const student = await this.student.findById(studentId);
-    if (!student) {
-      return new apiResponse(404, null, "Student not found");
-    }
+import asyncHandler from "../utils/asyncHandler";
+import userServices from "./userServices";
 
-    // Lock personal and academic info
-    student.personalInfo.isLocked = true;
-    student.academics.isLocked = true;
-
-    // Add verification metadata
-    student.verificationStatus = "verified";
-    student.verifiedBy = adminId;
-    student.verificationDate = new Date();
-
-    const updatedStudent = await student.save();
-   
-    return new apiResponse(200, updatedStudent, "Student info locked and verified");
-  } catch (error) {
-    return new apiResponse(500, null, error.message);
+export default class AdminServices {
+  constructor() {
+    this.userServices = new userServices();
   }
-}
+  verifyandLockUser = asyncHandler(async (userId, adminId) => {
+    return await this.userServices.verifyandLockUser(userId, adminId);
+  });
+  unlockUserProfile = asyncHandler(async (userId, adminId) => {
+    return await this.userServices.unlockUserProfile(userId, adminId);
+  });
+  demoteAdmin = asyncHandler(async (userId) => {
+    return await this.userServices.demoteAdmin(userId);
+  });
+  promoteAdmin = asyncHandler(async (userId, userRoleAsAdmin) => {
+    return await this.userServices.promoteAdmin(userId, userRoleAsAdmin);
+  });
 }
