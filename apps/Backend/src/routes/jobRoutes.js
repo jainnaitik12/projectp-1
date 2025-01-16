4a67b2018afe5450db43a40728283ea0b91cb946
udaynarwal72 <udaynarwal72@gmail.com>
2025-01-16 19:31:24 +0530

    new update

--- apps/Backend/src/routes/jobRoutes.js
+++ apps/Backend/src/routes/jobRoutes.js
@@ -5,37 +5,36 @@ import { Router } from "express";
 // import  auth, {authorizeRoles } from "../middlewares/auth.middlewares.js";
 
 const jobRoutes = Router();
-// const jobModel = new JobModel();
-// const jobService = new JobService(jobModel);
-// const jobController = new JobController(jobService);
 
+//getting all job
+jobRoutes.get("/bulk", (req, res) => {
 
-// jobRouter.get("/bulk", (req, res) => {
-//     jobController.getAllJobs(req,res);
-// });
+});
 
-// jobRouter.post("/create/:id", auth, authorizeRoles('admin'),(req, res) => {
-//     jobController.createJob(req,res);
-// })
+//posting job
+jobRoutes.post('/create', (req, res) => {
 
-// jobRouter.delete("/delete/:id", auth, authorizeRoles('admin'),(req, res) => {
-//     jobController.deleteJob(req,res);
-// })
+});
 
-// jobRouter.put("/update/:id", auth, authorizeRoles('admin'),(req, res) => {
-//     jobController.updateJob(req,res);
-// })
+//deleting job
+jobRoutes.delete("/delete/:id", (req, res) => {
 
-// jobRouter.get("/getjob/:id", (req, res) => {
-//     jobController.getJobById(req,res);
-// })
+});
 
-// jobRouter.put("/activejob/:id", auth, authorizeRoles('admin'),(req, res) => {
-//     jobController.activateJob(req,res);
-// })
+//updating job
+jobRoutes.put("/update/:id", (req, res) => {
 
-// jobRouter.put("/closejob/:id", auth, authorizeRoles('admin'),(req, res) => {
-//     jobController.closeJob(req,res);
-// })
+});
+
+//get job by id
+jobRoutes.get("/getjob/:id", (req, res) => {
+
+});
+
+//make a job active
+jobRoutes.put("/activejob/:id", (req, res) => {
+
+});
+//make a 
 
 export default jobRoutes;
